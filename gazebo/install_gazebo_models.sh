86bf4b1a0b3631bedb6dc20ffe18d8973e9c3d43
rojas70 <rojas70@gmail.com>
2020-11-21 15:46:28 +0800

    updated repo address from bitbutcket to github

--- gazebo/install_gazebo_models.sh
+++ gazebo/install_gazebo_models.sh
@@ -1,16 +1,16 @@
 #!/bin/sh
 
 # Download all model archive files
-hg clone https://bitbucket.org/osrf/gazebo_models
+git clone https://github.com/osrf/gazebo_models.git
 
-# # This is the folder into which wget downloads the model archives
-# cd gazebo_models
+# This is the folder into which wget downloads the model archives
+cd gazebo_models
 
-# # Extract all model archives
-# for i in *
-# do
-#   tar -zvxf "$i/model.tar.gz"
-# done
+# Extract all model archives
+for i in *
+do
+  tar -zvxf "$i/model.tar.gz"
+done
 
-# # Copy extracted files to the local model folder
+# Copy extracted files to the local model folder
 # cp -vfR * "$HOME/.gazebo/models/"
